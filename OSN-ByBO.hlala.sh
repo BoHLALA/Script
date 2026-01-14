32a58427881d441cc6c6c7d683b1b669784e1d82
BO-hlala <bo-hlala@hotmail.com>
2026-01-14 03:01:30 +0300

    Add files via upload

---
+++ OSN-ByBO.hlala.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#
+
+# Slyk Universal toppicks V5 (C) kiddac. 2019
+
+python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/OSN-ByBO.hlala.py
+
+# Slyk Universal toppicks V5 (C) kiddac. 2019
+echo 1 > /proc/sys/vm/drop_caches
+echo 2 > /proc/sys/vm/drop_caches
+echo 3 > /proc/sys/vm/drop_caches
+
+python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/picker1.py
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/*.json 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "##################################################"
+echo "#         Developed By Bo.HLALA .. ^_^            #"
+echo "#     Updated Successfully ... Lets Fun !!!       #"
+echo "#         Thanks for using our script             #"
+echo "##################################################"
+else
+echo "   >>>>   INSTALLATION FAILED !   <<<<"
+fi;
+echo '**************************************************'
+echo '**                   FINISHED                   **'
+echo '**************************************************'
+
+exit 0
