9d1811c606b2de398196731fb1b42a89e1172115
BO-hlala <bo-hlala@hotmail.com>
2026-01-17 03:20:27 +0300

    Update sky-Bohlala1.sh

--- sky-Bohlala1.sh
+++ sky-Bohlala1.sh
@@ -8,9 +8,8 @@ echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
 
 if test -f /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/OSN_channels_data.json; then
-    python usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/slyk/sky-ByBO.hlala.py
+    python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/OSN-ByBO.hlala.py
 fi
-
 
 
 
