7a0aa4936b6a007ddea1350531b8fc72d3d44ab0
BO-hlala <bo-hlala@hotmail.com>
2026-01-17 03:05:34 +0300

    Add files via upload

---
+++ sky-Bohlala1.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+# Slyk Universal toppicks V5 (C) kiddac. 2019
+# Slyk Universal toppicks V5 (C) By BO.hlala. 2026
+
+python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/sky_scraper.py
+
+# Slyk Universal toppicks V5 (C) By BO.hlala. 2026
+echo 1 > /proc/sys/vm/drop_caches
+echo 2 > /proc/sys/vm/drop_caches
+echo 3 > /proc/sys/vm/drop_caches
+
+python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/slyk/sky-ByBO.hlala.py
+fi
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/*.json 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "##################################################"
+echo "#         Developed By Bo.HLALA .. ^_^            #"
+echo "#     Updated Successfully ... Lets Fun !!!       #"
+echo "#         Thanks for using our script             #"
+echo "##################################################"
+else
+echo "   >>>>   INSTALLATION FAILED !   <<<<"
+fi;
+echo '**************************************************'
+echo '**                   FINISHED                   **'
+echo '**************************************************'
+
+exit 0
