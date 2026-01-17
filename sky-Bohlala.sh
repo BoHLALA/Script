dc6ae92103cb96b60070ac4794fd9013406ea5ac
BO-hlala <bo-hlala@hotmail.com>
2026-01-17 03:22:01 +0300

    Update sky-Bohlala.sh

--- sky-Bohlala.sh
+++ sky-Bohlala.sh
@@ -10,7 +10,7 @@ echo 1 > /proc/sys/vm/drop_caches
 echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
 
-python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/slyk/sky-ByBO.hlala.py
+python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/OSN_channels_data.json; then
 fi
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/*.json 
 
