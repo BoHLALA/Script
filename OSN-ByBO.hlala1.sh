511dae2bbc2f6a37fc6e681cc227c6574a32e28e
BO-hlala <bo-hlala@hotmail.com>
2026-01-21 02:43:57 +0300

    Update OSN-ByBO.hlala1.sh

--- OSN-ByBO.hlala1.sh
+++ OSN-ByBO.hlala1.sh
@@ -2,7 +2,7 @@
 #
 # Slyk Universal toppicks V5 (C) kiddac. 2019
 # Slyk Universal toppicks V5 (C) By BO.hlala. 2026
-
+python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/OSN_scraper1.py
 echo 1 > /proc/sys/vm/drop_caches
 echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
