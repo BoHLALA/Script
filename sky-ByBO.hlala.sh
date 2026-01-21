5b5572fbd3591fb9c7d47b72181d624d85da73c5
BO-hlala <bo-hlala@hotmail.com>
2026-01-21 06:09:57 +0300

    Update echo messages in sky-ByBO.hlala.sh

--- sky-ByBO.hlala.sh
+++ sky-ByBO.hlala.sh
@@ -42,11 +42,11 @@ if test -f /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk
 
 
 
-echo "##################################################"
-echo "#         Developed By Bo.HLALA .. ^_^            #"
-echo "#     Updated Successfully ... Lets Fun !!!       #"
-echo "#         Thanks for using our script             #"
-echo "##################################################"
+echo "**************************************************"
+echo "          Developed By Bo.HLALA .. ^_^ 👌        "
+echo "      Updated Successfully ... Lets Fun !!!       "
+echo "           Thanks for using our script            "
+echo "**************************************************"
 else
 echo "   >>>>   INSTALLATION FAILED !   <<<<"
 fi;
