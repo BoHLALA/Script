5bc4ac9c73c692b756f674053b6d7e9bbdbe1fac
BO-hlala <bo-hlala@hotmail.com>
2026-01-21 03:06:52 +0300

    Update OSN-ByBO.hlala1.sh

--- OSN-ByBO.hlala1.sh
+++ OSN-ByBO.hlala1.sh
@@ -3,7 +3,7 @@
 # Slyk Universal toppicks V5 (C) kiddac. 2019
 # Slyk Universal toppicks V5 (C) ByBO.hlala. 2026
 
-python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/OSN_scraper1.py
+python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/kids_scraper.py
 
 # Slyk Universal toppicks V5 (C) ByBO.hlala. 2026
 echo 1 > /proc/sys/vm/drop_caches
@@ -11,7 +11,7 @@ echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
 
 if test -f /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/OSN_channels_data.json; then
-    python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/OSN-ByBO.hlala1.py
+    python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/kids-ByBO.hlala.py
 fi
 
 
