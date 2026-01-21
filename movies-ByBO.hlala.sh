56de295578034581dde67d1b1d73fff9c96a7c03
BO-hlala <bo-hlala@hotmail.com>
2026-01-21 05:52:51 +0300

    Update movies-ByBO.hlala.sh

--- movies-ByBO.hlala.sh
+++ movies-ByBO.hlala.sh
@@ -42,10 +42,10 @@ fi
 
 
 echo "                                                   "
-echo "        Developed By Bo.HLALA .. ^_^ 👌          " 
-echo "#                                                 #"
-echo "#     Updated Successfully ... Lets Fun !!!       #"
-echo "#         Thanks for using our script             #"
+echo "        Developed By Bo.HLALA .. ^_^ 👌 🎨        " 
+echo "                                                   "
+echo "      Updated Successfully ... Lets Fun !!!        "
+echo "          Thanks for using our script              "
 echo "**************************************************"
 else
 echo "   >>>>   INSTALLATION FAILED !   <<<<"
