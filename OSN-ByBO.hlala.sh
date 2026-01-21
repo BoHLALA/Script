7e04119be17c7703164f4254445495a957ecb410
BO-hlala <bo-hlala@hotmail.com>
2026-01-21 06:08:44 +0300

    Update developer message to include emoji

--- OSN-ByBO.hlala.sh
+++ OSN-ByBO.hlala.sh
@@ -44,7 +44,7 @@ fi
 
 
 echo "**************************************************"
-echo "          Developed By Bo.HLALA .. ^_^            "
+echo "          Developed By Bo.HLALA .. ^_^ 👌        "
 echo "      Updated Successfully ... Lets Fun !!!       "
 echo "           Thanks for using our script            "
 echo "**************************************************"
