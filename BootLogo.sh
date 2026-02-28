39c4c2e845d75ec1b73512c5e8bcb0a62f056c5f
BO-hlala <bo-hlala@hotmail.com>
2026-03-01 01:00:05 +0300

    Update BootLogo.sh

--- BootLogo.sh
+++ BootLogo.sh
@@ -13,8 +13,8 @@ echo $MY_SEP
 echo 'Downloading '$MY_FILE' ...'
 echo $MY_SEP
 echo ''
-
-wget -O /var/volatile/tmp/BootLogo.tar.gz --no-check-certificate "https://raw.githubusercontent.com/BoHLALA/Ksa/main/BootLogo.tar.gz"
+wget -O /var/volatile/tmp/BootLogo.tar.gz --no-check-certificate "https://github.com/BoHLALA/Ksa/releases/download/BootLogo_v1/BootLogo.tar.gz"
+#wget -O /var/volatile/tmp/BootLogo.tar.gz --no-check-certificate "https://raw.githubusercontent.com/BoHLALA/Ksa/main/BootLogo.tar.gz"
 
 rm -rf "/usr/share/enigma2/BoHLALA_FHD/k.s.a/Bootlogo"
 
@@ -62,4 +62,5 @@ else
 fi
 # ----------------------------------------------------------------------------------------------------------
 fi
+
 
