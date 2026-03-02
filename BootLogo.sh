2dd07286d62b778967cbcc26acca583e44d0c325
BO-hlala <bo-hlala@hotmail.com>
2026-03-02 22:53:30 +0300

    Update BootLogo.sh

--- BootLogo.sh
+++ BootLogo.sh
@@ -1,87 +1,84 @@
 #!/bin/sh
-#########################################################
-version= 2.4
-description= BoHlala V1.2L\n Skin BoHlala ready to upgrade... Lets Fun !!!
+ description= Developed By Bo.HLALA .. ^_^  ready to upgrade... Lets Fun !!!
+# ==============================================
+# SCRIPT : DOWNLOAD AND INSTALL xstreamity #
+# =====================================================================================================================
+# Command: wget https://raw.githubusercontent.com/BoHLALA/Script/K.S.A/KiddaC_Skin_E2sentials.sh -O - | /bin/sh #
+# =====================================================================================================================
+wget -O /var/volatile/tmp/BootLogo.tar.gz --no-check-certificate "https://github.com/BoHLALA/Ksa/main/BootLogo.tar.gz"
 
-#########################################################
+#rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD"
+#rm -rf "/usr/share/enigma2/BoHLALA_FHD"
+########################################################################################################################
+# Plugin	... Enter Manually
+########################################################################################################################
 
-initializeANSI()
-{
-  esc=""
+PACKAGE_DIR='Plugins/K.S.A'
 
-  redf="${esc}[31m";     greenf="${esc}[32m"
-  yellowf="${esc}[33m"   bluef="${esc}[34m";   
-  cyanf="${esc}[36m";    purplef="${esc}[35m"   
-  
-  redfbright="${esc}[91m";     greenfbright="${esc}[92m"
-  yellowfbright="${esc}[93m"   bluefbright="${esc}[94m";   
-  cyanfbright="${esc}[96m";    purplefbright="${esc}[95m"   
-                      
-  boldon="${esc}[1m";    
-  reset="${esc}[0m"
-}
+MY_FILE="BootLogo.tar.gz"
 
-# note in this first use that switching colors doesn't require a reset
-# first - the new color overrides the old one.
+########################################################################################################################
+# Auto ... Do not change
+########################################################################################################################
 
-initializeANSI
+# Decide : which package ?
+MY_MAIN_URL="https://raw.githubusercontent.com/BoHLALA/"
+if which dpkg > /dev/null 2>&1; then
+	MY_FILE=$MY_DEB
+	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_DEB
+else
+	MY_FILE=$MY_IPK
+	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_IPK
+fi
+MY_TMP_FILE="/tmp/"$MY_FILE
 
-cat << EOF 
 
-${boldon}${redfbright}■■■■■■■■ ${boldon}${greenfbright}  ■■■■■■■■ ${boldon}${yellowfbright}        ■  ${boldon}${bluefbright}      ■■■        ■■■ ${boldon}${redfbright}  ■■■■      ■■ ${boldon}${greenfbright}  ■■■■ ${boldon}${yellowfbright}  ■■■■■■■■ ${boldon}${bluefbright}  ■■■■■■ ${boldon}${purplefbright}      ■■■■ ${reset}
-${boldon}${redfbright}■  ■■  ■ ${boldon}${greenfbright}   ■■      ${boldon}${yellowfbright}       ■■■ ${boldon}${bluefbright}         ■■    ■■    ${boldon}${redfbright}     ■■     ■■ ${boldon}${greenfbright}   ■■  ${boldon}${yellowfbright}  ■  ■■  ■ ${boldon}${bluefbright}   ■■   ■■ ${boldon}${purplefbright}  ■■    ■■ ${reset}
-${boldon}${redfbright}   ■■    ${boldon}${greenfbright}   ■■      ${boldon}${yellowfbright}      ■■■■■${boldon}${bluefbright}       ■■  ■■■■  ■■  ${boldon}${redfbright}   ■■ ■■    ■■ ${boldon}${greenfbright}   ■■  ${boldon}${yellowfbright}     ■■    ${boldon}${bluefbright}   ■■    ■■ ${boldon}${purplefbright} ■■    ■■ ${reset}
-${boldon}${redfbright}   ■■    ${greenf}   ■■■■■■ ${yellowf}      ■(   )■   ${bluef}   ■■   ■■   ■■ ${redf}    ■■  ■■   ■■ ${greenf}   ■■ ${yellowf}      ■■ ${bluef}      ■■   ■■ ${purplef}  ■■    ■■ ${reset}
-${boldon}${redfbright}   ■■    ${greenf}   ■■     ${yellowf}     ■■■■ ■■■■  ${bluef}   ■■        ■■ ${redf}    ■■   ■■  ■■ ${greenf}   ■■ ${yellowf}      ■■ ${bluef}      ■■ ■■   ${purplef}  ■■    ■■ ${reset}
-${boldon}${redfbright}   ■■    ${greenf}   ■■     ${yellowf}    ■■       ■■ ${bluef}   ■■        ■■ ${redf}    ■■    ■■    ${greenf}   ■■ ${yellowf}      ■■ ${bluef}      ■■   ■■ ${purplef}  ■■    ■■ ${reset}
-${boldon}${redfbright}  ■■■■   ${greenf}  ■■■■■■■■${yellowf}   ■■         ■■${bluef}   ■■        ■■ ${redf}    ■■     ■■■■ ${greenf}  ■■■■${yellowf}     ■■■■${bluef}     ■■■■   ■■${purplef}    ■■■■ ${reset}
 
-${boldon}${yellowfbright}				    TeamNitro Skin BoHLALA V2.3 ${reset}
+        echo "###################################################################"
+        echo "#       KiddaC-Skin-E2sentials INSTALLED SUCCESSFULLY             #"
+        echo "#                     Developed By Bo.HLALA                       #"
+        echo "#            https://www.tunisia-sat.com/forums/forums            #"
+        echo "#                https://www.linuxsat-support.com/                #"
+        echo "###################################################################"
+        echo "#        Sucessfully Download skin ... Lets Fun !!!  .. ^_^       #"
+        echo "###################################################################"
 
-EOF
 
-#########################################################
-MY_FILE1="BootLogo.tar.gz"
-MY_TMP_FILE1="/var/volatile/tmp/"$MY_FILE1
-#########################################################
 
+# Remove previous file (if any)
+rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+# Download package file
 MY_SEP='============================================================='
 echo $MY_SEP
 echo 'Downloading '$MY_FILE' ...'
 echo $MY_SEP
-echo 'Downloading '$MY_FILE1' ...'
-echo $MY_SEP
 echo ''
+wget -T 2 $MY_URL -P "/tmp/"
 
-wget -O /var/volatile/tmp/BootLogo.tar.gz --no-check-certificate "https://github.com/BoHLALA/Ksa/main/BootLogo.tar.gz"
-
-rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD"
-rm -rf "/usr/share/enigma2/BoHLALA_FHD"
-
-
-if [ -f $MY_TMP_FILE ]; [ -f $MY_TMP_FILE1 ]; then
-
+# Check download
+if [ -f $MY_TMP_FILE ]; then
+	# Install
 	echo ''
 	echo $MY_SEP
-	echo 'Extracting ...'
+	echo 'Installation started'
 	echo $MY_SEP
 	echo ''
-	tar -xzvf $MY_TMP_FILE -C /
-	tar -xzvf $MY_TMP_FILE1 -C /
+	if which dpkg > /dev/null 2>&1; then
+		dpkg -i --force-overwrite $MY_TMP_FILE
+		apt install -f -y
+	else
+		opkg install --force-reinstall $MY_TMP_FILE
+	fi
 	MY_RESULT=$?
 
-	rm -f $MY_TMP_FILE > /dev/null 2>&1
-	rm -f $MY_TMP_FILE1 > /dev/null 2>&1
-
+	# Result
+	echo ''
 	echo ''
 	if [ $MY_RESULT -eq 0 ]; then
-        echo "########################################################################"
-        echo "#          TeamNitro By BoHLALA v_1.2 INSTALLED SUCCESSFULLY            s#"
-        echo "#                      BY TeamNitro - support on                       #"
-        echo "#              https://www.tunisia-sat.com/forums/forums               #"
-        echo "########################################################################"
-        echo "#        Sucessfully Download Please apply from skin selection         #"
-        echo "########################################################################"
+		echo "   >>>>   SUCCESSFULLY INSTALLED   <<<<"
+		echo ''
+		echo "   >>>>         RESTARING         <<<<"
 		if which systemctl > /dev/null 2>&1; then
 			sleep 2; systemctl restart enigma2
 		else
@@ -92,14 +89,16 @@ if [ -f $MY_TMP_FILE ]; [ -f $MY_TMP_FILE1 ]; then
 	else
 		echo "   >>>>   INSTALLATION FAILED !   <<<<"
 	fi;
-	 echo '**************************************************'
-	 echo '**                   FINISHED                   **'
-	 echo '**************************************************'
-	 echo ''
-	 exit 0
+	echo ''
+	echo '**************************************************'
+	echo '**                   FINISHED                   **'
+	echo '**************************************************'
+	echo ''
+	exit 0
 else
-	 echo ''
-	 echo "Download failed !"
-	 exit 1
+	echo ''
+	echo "Download failed !"
+	exit 1
 fi
-# ----------------------------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------------------------------
