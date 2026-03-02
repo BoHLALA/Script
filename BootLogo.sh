e767dd4847a8f8f02910ce9d27d1f5e196f5dffa
BO-hlala <bo-hlala@hotmail.com>
2026-03-03 01:57:15 +0300

    Update BootLogo.sh

--- BootLogo.sh
+++ BootLogo.sh
@@ -1,72 +1,23 @@
 #!/bin/sh
-#########################################################
-version= 2.2
-description= Developed By Bo.HLALA .. ^_^  ready to upgrade... Lets Fun !!!
-#########################################################
-#########################################################
-MY_FILE="BootLogo.tar.gz"
-MY_TMP_FILE="/var/volatile/tmp/"$MY_FILE
-#########################################################
 
-MY_SEP='============================================================='
-echo $MY_SEP
-echo 'Downloading '$MY_FILE' ...'
-echo $MY_SEP
-echo 'Downloading '$MY_FILE1' ...'
-echo $MY_SEP
-echo ''
+URL="https://raw.githubusercontent.com/BoHLALA/Ksa/main/BootLogo.tar.gz"
+FILE="/tmp/BootLogo.tar.gz"
 
-wget -O /var/volatile/tmp/BootLogo.tar.gz --no-check-certificate "https://raw.githubusercontent.com/BoHLALA/Ksa/main/BootLogo.tar.gz"
+rm -f "$FILE"
+wget "$URL" -O "$FILE"
 
+if [ -s "$FILE" ]; then
+    tar -xzvf "$FILE" -C /
+    rm -f "$FILE"
 
-if [ -f $MY_TMP_FILE ] && [ -f $MY_TMP_FILE1 ]; then
+    echo "BootLogo Installed Successfully."
+    echo "By: قــدام (BoHLALA)"
 
-	echo ''
-	echo $MY_SEP
-	echo 'Extracting ...'
-	echo $MY_SEP
-	echo ''
-	tar -xzvf $MY_TMP_FILE -C /
-	tar -xzvf $MY_TMP_FILE1 -C /
-	MY_RESULT=$?
-
-	rm -f $MY_TMP_FILE > /dev/null 2>&1
-	rm -f $MY_TMP_FILE1 > /dev/null 2>&1
-
-	echo ''
-	if [ $MY_RESULT -eq 0 ]; then
- 
-        echo "###################################################################"
-        echo "#         skin BoHLALA_FHD v_2.2 INSTALLED SUCCESSFULLY           #"
-        echo "#                     Developed By Bo.HLALA                       #"
-        echo "#            https://www.tunisia-sat.com/forums/forums            #"
-        echo "#                https://www.linuxsat-support.com/                #"
-        echo "###################################################################"
-        echo "#        Sucessfully Download skin ... Lets Fun !!!  .. ^_^       #"
-        echo "###################################################################"	
- 
-		if which systemctl > /dev/null 2>&1; then
-			sleep 2; systemctl restart enigma2
-		else
-			init 4
-			sleep 4 > /dev/null 2>&1
-			init 3
-		fi
-	else
-		echo "   >>>>   INSTALLATION FAILED !   <<<<"
-	fi
-	 echo '**************************************************'
-	 echo '**                   FINISHED                   **'
-	 echo '**************************************************'
-	 echo ''
-	 exit 0
+    if which systemctl > /dev/null 2>&1; then
+        sleep 2; systemctl restart enigma2
+    else
+        init 4; sleep 4; init 3
+    fi
 else
-	 echo ''
-	 echo "Download failed !"
-	 exit 1
+    echo "Download failed."
 fi
-# ----------------------------------------------------------------------------------------------------------
-fi
-
-
-
