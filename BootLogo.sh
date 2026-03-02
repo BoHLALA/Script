1bfae19a4e2d8ea85c5d1929994e2754c4f8af8b
BO-hlala <bo-hlala@hotmail.com>
2026-03-02 22:44:11 +0300

    Update BootLogo.sh

--- BootLogo.sh
+++ BootLogo.sh
@@ -1,23 +1,105 @@
 #!/bin/sh
+#########################################################
+version= 2.4
+description= BoHlala V1.2L\n Skin BoHlala ready to upgrade... Lets Fun !!!
 
-URL="https://raw.githubusercontent.com/BoHLALA/Ksa/main/BootLogo.tar.gz"
-FILE="/tmp/BootLogo.tar.gz"
+#########################################################
 
-rm -f "$FILE"
-wget "$URL" -O "$FILE"
+initializeANSI()
+{
+  esc=""
 
-if [ -s "$FILE" ]; then
-    tar -xzvf "$FILE" -C /
-    rm -f "$FILE"
+  redf="${esc}[31m";     greenf="${esc}[32m"
+  yellowf="${esc}[33m"   bluef="${esc}[34m";   
+  cyanf="${esc}[36m";    purplef="${esc}[35m"   
+  
+  redfbright="${esc}[91m";     greenfbright="${esc}[92m"
+  yellowfbright="${esc}[93m"   bluefbright="${esc}[94m";   
+  cyanfbright="${esc}[96m";    purplefbright="${esc}[95m"   
+                      
+  boldon="${esc}[1m";    
+  reset="${esc}[0m"
+}
 
-    echo "BootLogo Installed Successfully."
-    echo "By: قــدام (BoHLALA)"
+# note in this first use that switching colors doesn't require a reset
+# first - the new color overrides the old one.
 
-    if which systemctl > /dev/null 2>&1; then
-        sleep 2; systemctl restart enigma2
-    else
-        init 4; sleep 4; init 3
-    fi
+initializeANSI
+
+cat << EOF 
+
+${boldon}${redfbright}■■■■■■■■ ${boldon}${greenfbright}  ■■■■■■■■ ${boldon}${yellowfbright}        ■  ${boldon}${bluefbright}      ■■■        ■■■ ${boldon}${redfbright}  ■■■■      ■■ ${boldon}${greenfbright}  ■■■■ ${boldon}${yellowfbright}  ■■■■■■■■ ${boldon}${bluefbright}  ■■■■■■ ${boldon}${purplefbright}      ■■■■ ${reset}
+${boldon}${redfbright}■  ■■  ■ ${boldon}${greenfbright}   ■■      ${boldon}${yellowfbright}       ■■■ ${boldon}${bluefbright}         ■■    ■■    ${boldon}${redfbright}     ■■     ■■ ${boldon}${greenfbright}   ■■  ${boldon}${yellowfbright}  ■  ■■  ■ ${boldon}${bluefbright}   ■■   ■■ ${boldon}${purplefbright}  ■■    ■■ ${reset}
+${boldon}${redfbright}   ■■    ${boldon}${greenfbright}   ■■      ${boldon}${yellowfbright}      ■■■■■${boldon}${bluefbright}       ■■  ■■■■  ■■  ${boldon}${redfbright}   ■■ ■■    ■■ ${boldon}${greenfbright}   ■■  ${boldon}${yellowfbright}     ■■    ${boldon}${bluefbright}   ■■    ■■ ${boldon}${purplefbright} ■■    ■■ ${reset}
+${boldon}${redfbright}   ■■    ${greenf}   ■■■■■■ ${yellowf}      ■(   )■   ${bluef}   ■■   ■■   ■■ ${redf}    ■■  ■■   ■■ ${greenf}   ■■ ${yellowf}      ■■ ${bluef}      ■■   ■■ ${purplef}  ■■    ■■ ${reset}
+${boldon}${redfbright}   ■■    ${greenf}   ■■     ${yellowf}     ■■■■ ■■■■  ${bluef}   ■■        ■■ ${redf}    ■■   ■■  ■■ ${greenf}   ■■ ${yellowf}      ■■ ${bluef}      ■■ ■■   ${purplef}  ■■    ■■ ${reset}
+${boldon}${redfbright}   ■■    ${greenf}   ■■     ${yellowf}    ■■       ■■ ${bluef}   ■■        ■■ ${redf}    ■■    ■■    ${greenf}   ■■ ${yellowf}      ■■ ${bluef}      ■■   ■■ ${purplef}  ■■    ■■ ${reset}
+${boldon}${redfbright}  ■■■■   ${greenf}  ■■■■■■■■${yellowf}   ■■         ■■${bluef}   ■■        ■■ ${redf}    ■■     ■■■■ ${greenf}  ■■■■${yellowf}     ■■■■${bluef}     ■■■■   ■■${purplef}    ■■■■ ${reset}
+
+${boldon}${yellowfbright}				    TeamNitro Skin BoHLALA V2.3 ${reset}
+
+EOF
+
+#########################################################
+MY_FILE1="BootLogo.tar.gz"
+MY_TMP_FILE1="/var/volatile/tmp/"$MY_FILE1
+#########################################################
+
+MY_SEP='============================================================='
+echo $MY_SEP
+echo 'Downloading '$MY_FILE' ...'
+echo $MY_SEP
+echo 'Downloading '$MY_FILE1' ...'
+echo $MY_SEP
+echo ''
+
+wget -O /var/volatile/tmp/BootLogo.tar.gz --no-check-certificate "https://github.com/BoHLALA/Ksa/main/BootLogo.tar.gz"
+
+rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD"
+rm -rf "/usr/share/enigma2/BoHLALA_FHD"
+
+
+if [ -f $MY_TMP_FILE ]; [ -f $MY_TMP_FILE1 ]; then
+
+	echo ''
+	echo $MY_SEP
+	echo 'Extracting ...'
+	echo $MY_SEP
+	echo ''
+	tar -xzvf $MY_TMP_FILE -C /
+	tar -xzvf $MY_TMP_FILE1 -C /
+	MY_RESULT=$?
+
+	rm -f $MY_TMP_FILE > /dev/null 2>&1
+	rm -f $MY_TMP_FILE1 > /dev/null 2>&1
+
+	echo ''
+	if [ $MY_RESULT -eq 0 ]; then
+        echo "########################################################################"
+        echo "#          TeamNitro By BoHLALA v_1.2 INSTALLED SUCCESSFULLY            s#"
+        echo "#                      BY TeamNitro - support on                       #"
+        echo "#              https://www.tunisia-sat.com/forums/forums               #"
+        echo "########################################################################"
+        echo "#        Sucessfully Download Please apply from skin selection         #"
+        echo "########################################################################"
+		if which systemctl > /dev/null 2>&1; then
+			sleep 2; systemctl restart enigma2
+		else
+			init 4
+			sleep 4 > /dev/null 2>&1
+			init 3
+		fi
+	else
+		echo "   >>>>   INSTALLATION FAILED !   <<<<"
+	fi;
+	 echo '**************************************************'
+	 echo '**                   FINISHED                   **'
+	 echo '**************************************************'
+	 echo ''
+	 exit 0
 else
-    echo "Download failed."
+	 echo ''
+	 echo "Download failed !"
+	 exit 1
 fi
+# ----------------------------------------------------------------------------------------------------------
